013f7d7482cb04d8f350f2a2b9e8c291ce0b794a
Greg White <gregwhite90@gmail.com>
2020-07-20 17:20:48 -0400

    Add a template React front-end.

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: null,
+        };
+    }
+
+    componentDidMount() {
+        this.getMessage();
+    }
+
+    getMessage = () => {
+        fetch('/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ query: "{ hello }"}),
+        })
+            .then(res => res.json())
+            .then(data => this.setState({message: data.data.hello}));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <p>
+                        Backend says: {this.state.message}
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
