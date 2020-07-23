36e1265ab1284e9613fbf0ba8cb3ff00fbd5904e
Greg White <gregwhite90@gmail.com>
2020-07-23 09:12:47 -0400

    Enable adding Babel plugins (needed for Relay)

---
+++ client/scripts/updateSchema.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const DogTrainingSchema = require('../../schema');
+const { printSchema } = require('../../node_modules/graphql');
+
+const schemaPath = path.resolve(__dirname, '../data/schema.graphql');
+fs.writeFileSync(schemaPath, printSchema(DogTrainingSchema));
+console.log('Wrote ' + schemaPath);
