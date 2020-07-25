22ef967a9d2e47225c0337966c900ac9aa822677
Greg White <gregwhite90@gmail.com>
2020-07-25 12:44:12 -0400

    Hold off on resolving the user promise in DB layer

--- business-logic/data.js
+++ business-logic/data.js
@@ -7,13 +7,13 @@ const db = require('../db');
  * Mutation functions
  */
 async function createHuman({name}, context) {
-    console.log(await context.user.catch(e => throw new Error(e)));
+    console.log(context.user);
     const { rows } = await db.query('INSERT INTO humans(name) VALUES ($1) RETURNING *', [name]);
     return rows[0];
 }
 
 async function addBreedToDog({dog_id, breed_id}, context) {
-    console.log(await context.user.catch(e => throw new Error(e)));
+    console.log(context.user);
     await db.query('INSERT INTO dog_breeds(dog_id, breed_id) VALUES ($1, $2)', [dog_id, breed_id]);
 }
 
@@ -21,7 +21,7 @@ async function addBreedToDog({dog_id, breed_id}, context) {
  * Query single node functions
  */
 async function getNode({id, tableName}, context) {
-    console.log(await context.user.catch(e => throw new Error(e)));
+    console.log(context.user);
     const { rows } = await db.query(`SELECT * FROM ${tableName} WHERE id=$1`, [id]);
     return rows[0];
 }
@@ -30,7 +30,7 @@ async function getNode({id, tableName}, context) {
  * Query all nodes functions
  */
 async function getNodes({tableName}, context) {
-    console.log(await context.user.catch(e => throw new Error(e)));
+    console.log(context.user);
     const { rows } = await db.query(`SELECT * FROM ${tableName}`);
     return rows;
 }
@@ -39,7 +39,7 @@ async function getNodes({tableName}, context) {
  * Query relations (connections) functions
  */
 async function getDogBreeds({id}, context) {
-    console.log(await context.user.catch(e => throw new Error(e)));
+    console.log(context.user);
     const { rows } = await db.query(`SELECT breed_id FROM dog_breeds WHERE dog_id=$1`, [id]);
     return rows;
 }
