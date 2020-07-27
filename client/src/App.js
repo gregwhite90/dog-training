0bf1c17ab32eed2a4f27449955fa4953abba870a
Greg White <gregwhite90@gmail.com>
2020-07-27 11:16:01 -0400

    Fix import omissions

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import environment from './lib/createRelayEnvironment';
 import {
     BrowserRouter,
     Switch,
     Route,
 } from 'react-router-dom';
 
-// Import higher level containers instead
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+import environment from './lib/createRelayEnvironment';
+
 import Navigation from './react-components/nav/Navigation';
 import Home from './react-components/pages/Home';
 import About from './react-components/pages/About';
