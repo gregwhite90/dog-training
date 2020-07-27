11593b4bb04e3279e8fbd7a225cac610c7cc95f4
Greg White <gregwhite90@gmail.com>
2020-07-27 12:35:56 -0400

    Get and pass an authentication token with each GraphQL request

--- client/src/lib/createRelayEnvironment.js
+++ client/src/lib/createRelayEnvironment.js
@@ -4,15 +4,21 @@ import {
     RecordSource,
     Store,
 } from 'relay-runtime';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function fetchQuery(
     operation,
     variables,
 ) {
+    const { getAccessTokenSilently } = useAuth0();
+    const token = getAccessTokenSilently().then(token => token);
+    // TODO: error-handling code
     return fetch('/graphql', {
         method: 'POST',
         headers: {
+            'Accept': 'Application/json',
             'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
         },
         body: JSON.stringify({
             query: operation.text,
